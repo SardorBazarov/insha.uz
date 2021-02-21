29eea62bd465832a497f4a9189d6130b1a71db6a
SardorBazarov <57259406+Spirit2000125@users.noreply.github.com>
2021-02-21 17:11:38 +0500

    Update CheckoutSteps.js

--- frontend/src/components/CheckoutSteps.js
+++ frontend/src/components/CheckoutSteps.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function CheckoutSteps(props) {
   return (
     <div className="row checkout-steps">
-      <div className={props.step1 ? 'active' : ''}>Sign-In</div>
-      <div className={props.step2 ? 'active' : ''}>Shipping</div>
-      <div className={props.step3 ? 'active' : ''}>Payment</div>
-      <div className={props.step4 ? 'active' : ''}>Place Order</div>
+      <div className={props.step1 ? 'active' : ''}><Link className="checkout-steps" to="/signin">Ro`yxatdan o`tish</Link></div>
+      <div className={props.step1 ? 'active' : ''}><Link className="checkout-steps" to="/cart">Savat</Link></div>
+      <div className={props.step2 ? 'active' : ''}><Link className="checkout-steps" to="/shipping">Yetkazib berish</Link></div>
+      <div className={props.step3 ? 'active' : ''}><Link className="checkout-steps" to="/payment">To`lov</Link></div>
+      <div className={props.step4 ? 'active' : ''}><Link className="checkout-steps" to="/placeorder">Buyurtmani joylashtirish</Link></div>
     </div>
   );
 }
