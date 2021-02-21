6cea119e6fb26510ce502b2c49d86441b117b9e1
SardorBazarov <57259406+Spirit2000125@users.noreply.github.com>
2021-02-21 16:55:49 +0500

    Update ShippingAddressScreen.js

--- frontend/src/screens/ShippingAddressScreen.js
+++ frontend/src/screens/ShippingAddressScreen.js
@@ -1,6 +1,152 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+impoimport React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { saveShippingAddress } from '../actions/cartActions';
+import CheckoutSteps from '../components/CheckoutSteps';
+
+export default function ShippingAddressScreen(props) {
+  const userSignin = useSelector((state) => state.userSignin);
+
+  const { userInfo } = userSignin;
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+  const [lat, setLat] = useState(shippingAddress.lat);
+  const [lng, setLng] = useState(shippingAddress.lng);
+  const userAddressMap = useSelector((state) => state.userAddressMap);
+  const { address: addressMap } = userAddressMap;
+
+  if (!userInfo) {
+    props.history.push('/signin');
+  }
+  const [fullName, setFullName] = useState(shippingAddress.fullName);
+  const [address, setAddress] = useState(shippingAddress.address);
+  const [city, setCity] = useState(shippingAddress.city);
+  const [postalCode, setPostalCode] = useState(shippingAddress.postalCode);
+  const [country, setCountry] = useState(shippingAddress.country);
+  const dispatch = useDispatch();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const newLat = addressMap ? addressMap.lat : lat;
+    const newLng = addressMap ? addressMap.lng : lng;
+    if (addressMap) {
+      setLat(addressMap.lat);
+      setLng(addressMap.lng);
+    }
+    let moveOn = true;
+    if (!newLat || !newLng) {
+      moveOn = window.confirm(
+        'Siz GeoLokatsiyangizni xaritadan topmadingiz. Davom ettirasizmi?'
+      );
+    }
+    if (moveOn) {
+      dispatch(
+        saveShippingAddress({
+          fullName,
+          address,
+          city,
+          postalCode,
+          country,
+          lat: newLat,
+          lng: newLng,
+        })
+      );
+      props.history.push('/payment');
+    }
+  };
+  const chooseOnMap = () => {
+    dispatch(
+      saveShippingAddress({
+        fullName,
+        address,
+        city,
+        postalCode,
+        country,
+        lat,
+        lng,
+      })
+    );
+    props.history.push('/map');
+  };
+  return (
+    <div>
+      <CheckoutSteps step1 step2></CheckoutSteps>
+      <form className="form" onSubmit={submitHandler}>
+        <div>
+          <h1>Yetkazish manzili</h1>
+        </div>
+        <div>
+          <label htmlFor="fullName">F I Sh</label>
+          <input
+            type="text"
+            id="fullName"
+            placeholder="Familiya Ism Sharifi"
+            value={fullName}
+            onChange={(e) => setFullName(e.target.value)}
+            required
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="address">Manzil</label>
+          <input
+            type="text"
+            id="address"
+            placeholder="Manzil nomi"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+            required
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="city">Shahar</label>
+          <input
+            type="text"
+            id="city"
+            placeholder="Shahar nomi"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            required
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="postalCode">Pochta ZIP raqami</label>
+          <input
+            type="text"
+            id="postalCode"
+            placeholder="Hududiy Pochta raqamingizni kiriting"
+            value={postalCode}
+            onChange={(e) => setPostalCode(e.target.value)}
+            required
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="country">Davlat</label>
+          <input
+            type="text"
+            id="country"
+            placeholder="Davlatni kiriting"
+            value={country}
+            onChange={(e) => setCountry(e.target.value)}
+            required
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="chooseOnMap">GeoLokatsiya</label>
+          <button type="button" onClick={chooseOnMap}>
+            Xaritadan topish
+          </button>
+        </div>
+        <div>
+          <label />
+          <button className="primary" type="submit">
+            Davom Ettirish
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+rt { saveShippingAddress } from '../actions/cartActions';
 import CheckoutSteps from '../components/CheckoutSteps';
 
 export default function ShippingAddressScreen(props) {
